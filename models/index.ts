ec7cd5b6f672e48fddeed29fd51634c69ca43c6d
Morris Chiu <morris@eui.money>
2024-05-21 20:34:39 +0800

    wip

--- models/index.ts
+++ models/index.ts
@@ -3,8 +3,8 @@ import User from './user';
 import 'dotenv/config';
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-const connectString: string = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-connect(connectString)
+// const connectString: string = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+// connect(connectString)
 
 export {
     User
