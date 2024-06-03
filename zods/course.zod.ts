d9542a8a15d55b42935f04d484c970f86645941c
hsinyun <ruby8941@gmail.com>
2024-06-04 01:36:54 +0800

    feat course zod add price,schedule

--- zods/course.zod.ts
+++ zods/course.zod.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
+import type { ObjectId } from 'bson';
 
 export const createZod = z
   .object({
+    coach: z.custom<ObjectId>(),
     name: z.string(),
     coverImage: z.string(),
     description: z.string(),
@@ -10,3 +12,22 @@ export const createZod = z
     startDate:z.string().date(),
     isActive:z.boolean(),
   });
+export const priceZod = z
+.object({
+  id: z.custom<ObjectId>().optional(),
+  course: z.custom<ObjectId>(),
+  count: z.number(),
+  price: z.number(),
+});
+export const editPriceZod = z.array(priceZod);
+export const scheduleZod = z
+.object({
+  id: z.custom<ObjectId>().optional(),
+  coach: z.custom<ObjectId>(),
+  course: z.custom<ObjectId>(),
+  count: z.number(),
+  price: z.number(),
+  startTime: z.string().datetime(),
+  endTime: z.string().datetime(),
+});
+export const editScheduleZod = z.array(scheduleZod);
