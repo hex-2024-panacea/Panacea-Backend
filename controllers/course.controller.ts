89c148477efaf1140a11e7c5fb0f869cbf3baa57
Ellis <pp840405@gmail.com>
2024-06-13 23:36:23 +0800

    fix:測試金流

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -231,8 +231,8 @@ export const purchaseCourse = handleErrorAsync(async (req, res, next) => {
 });
 
 export const spgatewayNotify = handleErrorAsync(async (req, res, next) => {
-  console.log('req', req);
   const response = Object.assign({}, req.body);
+  console.log('req.body', req.body);
   console.log('response.TradeInfo', response);
   const thisShaEncrypt = createMpgShaEncrypt(response.TradeInfo);
   // 使用 HASH 再次 SHA 加密字串，確保比對一致（確保不正確的請求觸發交易成功）
