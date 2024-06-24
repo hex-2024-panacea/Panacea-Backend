9497a273dbb3004b3e0710ff625d019fea11ae4a
hsinyun <ruby8941@gmail.com>
2024-06-25 01:30:35 +0800

    fix bookingCourse controller userCreate

--- controllers/bookingCourse.controller.ts
+++ controllers/bookingCourse.controller.ts
@@ -237,10 +237,11 @@ export const userCreate = handleErrorAsync(async (req, res, next) => {
   //check order can book
   const existOrder = await OrderModel.findOne({
     _id: courseId,
-    user: userId,
+    userId: userId,
   });
   if (existOrder) {
     const remainCount = parseInt(existOrder.remainingCount);
+    const bookingCount = parseInt(existOrder.bookingCount);
     if (!isNaN(remainCount) && remainCount > 0) {
       //check courseSchedule can book
       const existCourseSchedule = await CourseScheduleModel.findOne({
@@ -251,7 +252,7 @@ export const userCreate = handleErrorAsync(async (req, res, next) => {
       if (existCourseSchedule) {
         const { startTime, endTime, coach } = existCourseSchedule;
         //create meeting url QQ
-        const meetingUrl = 'meeting';
+        const meetingUrl = 'meetingUrl';
         const booking = await BookingCourseModel.create({
           user: userId,
           course: courseId,
@@ -262,6 +263,15 @@ export const userCreate = handleErrorAsync(async (req, res, next) => {
           order: orderId,
           meetingUrl,
         });
+        await OrderModel.findOneAndUpdate(
+          {
+            _id: courseId,
+          },
+          {
+            remainingCount: (remainCount - 1).toString(),
+            bookingCount: (bookingCount + 1).toString(),
+          },
+        );
         if (booking) {
           return handleSuccess(res, 200, 'book success');
         }
