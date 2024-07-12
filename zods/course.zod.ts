d79779190b0405a639bb1b1aaa50cdad002a3d77
hsinyun <ruby8941@gmail.com>
2024-07-12 19:33:25 +0800

    fix course.zod.ts

--- zods/course.zod.ts
+++ zods/course.zod.ts
@@ -6,8 +6,8 @@ export const createZod = z.object({
   name: z.string(),
   coverImage: z.string(),
   description: z.string(),
-  category: z.string().array().min(1),
-  subCategory: z.string().array().min(1),
+  category: z.string().array().min(1).optional(),
+  subCategory: z.string().array().min(1).optional(),
   startDate: z.string().date(),
   isActive: z.boolean(),
 });
